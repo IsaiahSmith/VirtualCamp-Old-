328f9bd075c5839bdae7dac26c60d936c550e76a
Isaiah Smith <smithij@rose-hulman.edu>
2016-01-28 04:10:01 -0500

    added starting code

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>VirtualCamp</title>
+		<link href="styles.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body id="index">
+		<div id="header">Virtual<span id="title">Camp</span></div>
+		<div id="login">
+			<h2>Welcome to My<span id="title">Camp</span></h2>
+			Username: <input type="text" name="user"><br>
+  			Password: <input type="text" name="password"><br>
+  			<a id="loginbtn" href="homepage.php">Login</a>
+		</div>
+	</body>
+	
+</html>
